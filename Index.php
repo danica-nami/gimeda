424b3f5b1c2b862a3ee5e50685cffe9d8102ebf4
danica-nami <53499847+danica-nami@users.noreply.github.com>
2019-07-31 09:32:01 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Index.html</title>
-<body>
+    <title>For Sale</title>
+<body style="background-color:lightpink;">
+<i>
+<b>
+<img src="123.png"style="width:50px;height:50px;>
+     <p style="font-size:100%;text-align:left;">Bear Clothing</p>
+    <hr>
+</hr>
+</b>
+</i> 
+    <img src="k.png"style="width:300px;height:300px;>
+    <p style="font-size:300%;text-align:center;">$19.99 (Up to 50% discount)</p>
+<p style="text-align:right;"> Inspired by the idea that everything should be as comfy as a tee,</p>
+<p style="text-align:right;">our new texture & thread collection combines truly touchable fabrics </p>
+<p style="text-align:right;"> with pulled-together shapes.Made of stretchy jazquard, this just </p>
+<p style="text-align:right;"> -a-touch-cropped top ties flatteringly to one side.</p>
+   
 
-<article>
-  <header>
-    <h1>Most important heading here</h1>
-    <h3>Less important heading here</h3>
-    <p>Some additional information here</p>
-  </header>
-  <p>Lorem Ipsum dolor set amet....</p>
-</article>
+    <p style="font-size:200%;text-align:center;">BEAR WOODS T-SHIRT</p>
+    
 
 
 
