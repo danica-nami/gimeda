444b3f0177e28a51eec9bd99dc915253fef4d164
danica-nami <53499847+danica-nami@users.noreply.github.com>
2019-07-31 14:21:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,92 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>For Sale</title>
+<title>index.html</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+
+header {
+  background-color: #666;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: gray;
+}
+
+
+nav {
+  float: left;
+  width: 30%;
+  height: 300px; 
+  background:#ccc;
+  padding: 20px;
+}
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+article {
+  float: left;
+  padding: 20px;
+  width: 70%;
+  background-color:#f1f1f1;
+  height: 300px;
+
+section:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+footer {
+  background-color:#777;
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+@media (max-width: 600px) {
+  nav, article {
+    width: 100%;
+    height: auto;
+
+
+</style>
+</head>
 <body>
-<i>
-<b>
-<img src="123.png"style="width:50px;height:50px;>
-     <p style="font-size:100%;text-align:left;">Bear Clothing</p>
-    <hr>
-</hr>
-</b>
-</i> 
-    <img src="k.png"style="width:300px;height:300px;>
-    <p style="font-size:300%;text-align:center;">$19.99 (Up to 50% discount)</p>
-<p style="text-align:right;"> Inspired by the idea that everything should be as comfy as a tee,</p>
-<p style="text-align:right;">our new texture & thread collection combines truly touchable fabrics </p>
-<p style="text-align:right;"> with pulled-together shapes.Made of stretchy jazquard, this just </p>
-<p style="text-align:right;"> -a-touch-cropped top ties flatteringly to one side.1</p>
+
+<h2 style="text-align:center;font-family:bookman old style">DANICA GIMEDA</h2>
+<p style="font-family:Arial Unicode MS;text-align:center;">sample template</p>
+
+<header>
+  <h2>AESTHETIC</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <li><a href="#">programming</a></li>
+      <li><a href="#">web</a></li>
+      <li><a href="#">I-comp</a></li>
+    </ul>
+  </nav>
+  
+  <article>
+    <h1>Aesthetic</h1>
+    <p>Aesthetics, or esthetics is a branch of philosophy that deals with the nature of art, beauty and taste and with the creation or appreciation of beauty.</p>
    
-<p style="font-size:200%;text-align:center;">BEAR WOODS T-SHIRT</p>
-    
-<footer>
-  <p>Posted by: Danica Gimeda</p>
-  <p>Contact information: <a href="monami.mandora01@gmail.com">
-  facebook.com</a>.</p>
+  </article>
+</section>
+
+<footer style="background-color:gray;">
+  <p style="text-align:center;">Nica.monami</p>
 </footer>
-
-
 
 </body>
 </html>
